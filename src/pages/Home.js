b6dc05fb414e8ac5e14b622e60956ec4c2e829fc
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-11-01 19:19:15 +0100

    Crear Entorn i Arquitectura del projecte

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import logo from '../assets/images/logo.svg';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <h1> Page d'accueil </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;

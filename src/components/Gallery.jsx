3773e662656c202f1ce4647fcd2a5fdb79d6b92a
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-11-09 10:08:21 +0100

    useState ?

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 import saloon from '../assets/images/sejour.webp'
 
 const Gallery = () => {

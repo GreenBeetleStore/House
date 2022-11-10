09c5cc82b0ace38687ecf01ad93674c2814b813f
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-11-10 16:25:50 +0100

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,14 +2,33 @@
 
 import React from 'react'
 import traits from '../mocks/traits.json'
-import DropDowns from './DropDowns'
+import { useState } from 'react'
+import ArrowUp from '../assets/icons/arrowUp.svg'
+import ArrowDown from '../assets/icons/arrowDown.svg'
 
 const Skills = () => {
+   /* DropDown 🏹*/
+   const [trigger, isTrigger] = useState(false)
+   const triggerStatus = () => {
+      isTrigger(!trigger)
+   }
+
    const traitsAbout = traits.map((trait) => (
-      <DropDowns>
-         <h3>{trait.titleTrait}</h3>
-         <p>{trait.skillTrait}</p>
-      </DropDowns>
+      <article className="K-Skills__article">
+         <div className="K-Skills__article__statement" onClick={triggerStatus}>
+            <h3>{trait.titleTrait}</h3>
+            {trigger ? (
+               <img src={ArrowUp} alt="Flèche" />
+            ) : (
+               <img src={ArrowDown} alt="Flèche" />
+            )}
+         </div>
+         {trigger && (
+            <div className="K-Skills__article__info">
+               <p>{trait.skillTrait}</p>
+            </div>
+         )}
+      </article>
    ))
    return <section className="K-Skills">{traitsAbout}</section>
 }

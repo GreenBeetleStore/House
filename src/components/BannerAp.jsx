2292d73d26b3175c6bcceb1e9d32f40837ff71b2
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-12-03 22:58:44 +0100

    CSS Responsive About (BannerAp, Skills)

--- src/components/BannerAp.jsx
+++ src/components/BannerAp.jsx
@@ -1,7 +1,7 @@
 /* BannerAp 🎏 src/components/BannerAp.jsx */
 
 import React from 'react'
-import aboutBanner from '../assets/images/banner02.webp'
+import aboutBanner from '../assets/images/bannerAp.webp'
 
 const BannerAp = () => {
    return (
@@ -9,7 +9,7 @@ const BannerAp = () => {
          <img
             src={aboutBanner}
             alt="Montagnes enneigées"
-            className="K-BannerAp __img"
+            className="K-BannerAp__img"
          />
       </section>
    )
